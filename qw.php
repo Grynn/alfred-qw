f24af6b90bd3b243e93a2d1f711f9a4a348cf222
Vishal Doshi <vishal.doshi@gmail.com>
2018-05-03 19:27:16 +0530

    Treat comma as an argument separator

--- qw.php
+++ qw.php
@@ -3,17 +3,14 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-$args = Clue\Arguments\split($argv[1]);
+$args = Clue\Arguments\split($argv[1], [ "," ]);
 $q1 = join("','", $args);
 
-echo '<?xml version="1.0"?>';
-echo <<<XML
-<output>
-<items>
-<item>
-	<title arg="$q1" valid="YES">'$q1'</title>
-	<text type="copy">'$q1'</text>
-</item>
-</items>
-</output>
-XML;
+//Output format: https://www.alfredapp.com/help/workflows/inputs/script-filter/json/
+$items[] = [
+	'title' => "'$q1'",
+	'text' => [ 'copy' => "'$q1'" ],
+];
+
+
+echo json_encode(compact('items'));
